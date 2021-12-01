4149061f697a2d89e5aa30690cef2aa36976f1d5
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2021-12-01 16:59:00 +0100

    solve second puzzle of day 1

--- src/test/kotlin/day01/Day01Tests.kt
+++ src/test/kotlin/day01/Day01Tests.kt
@@ -6,16 +6,47 @@ import utils.solvePuzzle
 
 fun solveFirstPuzzle(input: Sequence<String>): Long =
     input.map(String::toLong)
-        .zipWithNext()
-        .fold(0L) { acc, pair ->
-            if (pair.first < pair.second) acc + 1 else acc
-        }
+        .increases()
+
+fun solveSecondPuzzle(input: Sequence<String>): Long =
+    input.map(String::toLong)
+        .windowed(3)
+        .map { it.sum() }
+        .increases()
+
+private fun Sequence<Long>.increases(): Long =
+    zipWithNext()
+        .count { it.first < it.second }
+        .toLong()
 
 class Day01Tests {
 
     @Test
-    internal fun `solve first sample`() {
-        val input = """
+    internal fun `solve first puzzle for sample`() {
+        val input = sample.lineSequence()
+
+        solveFirstPuzzle(input) shouldBe 7L
+    }
+
+    @Test
+    internal fun `solve first puzzle`() {
+        solvePuzzle(1) { solveFirstPuzzle(it) } shouldBe 1529L
+    }
+
+    @Test
+    internal fun `solve second puzzle for sample`() {
+        val input = sample.lineSequence()
+
+        solveSecondPuzzle(input) shouldBe 5L
+    }
+
+    @Test
+    internal fun `solve second puzzle`() {
+        solvePuzzle(1) { solveSecondPuzzle(it) } shouldBe 1567L
+    }
+}
+
+val sample = """
             199
             200
             208
@@ -26,14 +57,4 @@ class Day01Tests {
             269
             260
             263"""
-            .trimIndent()
-            .lineSequence()
-
-        solveFirstPuzzle(input) shouldBe 7L
-    }
-
-    @Test
-    internal fun `solve first puzzle`() {
-        solvePuzzle(1) { solveFirstPuzzle(it) } shouldBe 1529L
-    }
-}
+    .trimIndent()
