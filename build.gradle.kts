f122864fbd1a7dadafd6150889c6deb4f6cc7131
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2021-12-07 08:48:36 +0100

    downgrade Kotlin version
    due to <https://youtrack.jetbrains.com/issue/KTIJ-20305>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.5.31"
     id("com.github.ben-manes.versions") version "0.39.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
 }
