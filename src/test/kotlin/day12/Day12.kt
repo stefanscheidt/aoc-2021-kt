99f6d73dac6aee193dc5bdeb71529bfc9678eff8
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2021-12-14 21:35:01 +0100

    solve day 12.

---
+++ src/test/kotlin/day12/Day12.kt
@@ -0,0 +1,56 @@
+package day12
+
+import utils.solvePuzzle
+
+typealias Cave = String
+
+val Cave.isBig: Boolean
+    get() = all { char -> char.isUpperCase() }
+
+// associates a cave with all its neighbours
+typealias CaveMap = Map<Cave, Set<Cave>>
+
+typealias Path = List<Cave>
+
+fun CaveMap.traverse(path: Path = listOf("start"), canVisit: (Cave, Path) -> Boolean): Set<Path> =
+    if (path.last() == "end") setOf(path)
+    else getValue(path.last())
+        .filter { cave -> canVisit(cave, path) }
+        .flatMap { cave -> traverse(path + cave, canVisit) }
+        .toSet()
+
+fun parseCaveMap(input: List<String>): CaveMap =
+    input.map { it.split("-") }
+        .flatMap {
+            val first = it.first()
+            val last = it.last()
+            listOf(first to last, last to first)
+        }.groupBy({ it.first }, { it.second })
+        .mapValues { (_, caves) -> caves.toSet() }
+
+fun solvePuzzle1(input: Sequence<String>): Long =
+    parseCaveMap(input.toList())
+        .traverse(canVisit = { cave, path -> cave !in path || cave.isBig })
+        .size.toLong()
+
+fun solvePuzzle2(input: Sequence<String>): Long {
+    fun noSmallCaveVisitedTwice(path: Path /* = kotlin.collections.List<day12.Cave /* = kotlin.String */> */): Boolean =
+        path.filterNot { it.isBig }.groupBy { it }.none { (_, visits) -> visits.size == 2 }
+
+    fun canVisit(cave: Cave, path: Path): Boolean =
+        when {
+            cave == "start" -> false
+            cave.isBig -> true
+            cave !in path -> true
+            else -> noSmallCaveVisitedTwice(path)
+        }
+
+    return parseCaveMap(input.toList())
+        .traverse(canVisit = ::canVisit)
+        .size.toLong()
+}
+
+fun main() {
+    println("Answer 1: ${solvePuzzle(12, ::solvePuzzle1)}")
+    println("Answer 2: ${solvePuzzle(12, ::solvePuzzle2)}")
+}
---
+++ src/test/kotlin/day12/Day12Tests.kt
@@ -0,0 +1,65 @@
+package day12
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day12Tests {
+
+    @Test
+    internal fun `parse cave map`() {
+        val input =
+            """
+                start-A
+                start-b
+                A-c
+                A-b
+                b-d
+                A-end
+                b-end
+            """.trimIndent()
+
+        parseCaveMap(input.lines()) shouldBe mapOf(
+            "start" to setOf("A", "b"),
+            "A" to setOf("start", "b", "c", "end"),
+            "b" to setOf("start", "A", "d", "end"),
+            "c" to setOf("A"),
+            "d" to setOf("b"),
+            "end" to setOf("A", "b")
+        )
+    }
+
+    @Test
+    fun `solve first puzzle with sample`() {
+        val input = sample.lineSequence()
+
+        solvePuzzle1(input) shouldBe 226L
+    }
+
+    @Test
+    fun `solve second puzzle with sample`() {
+        val input = sample.lineSequence()
+
+        solvePuzzle2(input) shouldBe 3509L
+    }
+
+    private val sample = """
+        fs-end
+        he-DX
+        fs-he
+        start-DX
+        pj-DX
+        end-zg
+        zg-sl
+        zg-pj
+        pj-he
+        RW-he
+        fs-DX
+        pj-RW
+        zg-RW
+        start-pj
+        he-WI
+        zg-he
+        pj-fs
+        start-RW
+    """.trimIndent()
+}
